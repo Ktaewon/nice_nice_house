503d0e7095fd567e027cae146c841b3f986d8204
Ktaewon <dydalehdxo@naver.com>
2021-12-10 01:10:44 +0900

    main page and map page added

--- app/src/main/java/org/cookandroid/nice_nice_house/MainActivity.kt
+++ app/src/main/java/org/cookandroid/nice_nice_house/MainActivity.kt
@@ -1,11 +1,18 @@
 package org.cookandroid.nice_nice_house
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btnMapOpen = findViewById<Button>(R.id.btnMapOpen)
+        btnMapOpen.setOnClickListener {
+            var intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
