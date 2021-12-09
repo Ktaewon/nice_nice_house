c2732b0ec897b68e7fd00242524f6e9f6ea150d5
Ktaewon <dydalehdxo@naver.com>
2021-12-10 00:09:57 +0900

    first init

---
+++ app/src/main/java/org/cookandroid/nice_nice_house/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.cookandroid.nice_nice_house
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
