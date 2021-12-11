68288e800111122732c0f4139be4c64589169bb1
Ktaewon <dydalehdxo@naver.com>
2021-12-12 01:59:24 +0900

    current location added

--- app/src/main/java/org/cookandroid/nice_nice_house/MapActivity.kt
+++ app/src/main/java/org/cookandroid/nice_nice_house/MapActivity.kt
@@ -68,7 +68,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val places_api_key = "AIzaSyAVEjRyS5VmNZmKS6iyXMrlddjZGnnFGF8"
+        val places_api_key = "API_KEY"
         setContentView(R.layout.map_main)
         supportActionBar!!.setDisplayShowHomeEnabled(false)
         if (savedInstanceState != null) {
